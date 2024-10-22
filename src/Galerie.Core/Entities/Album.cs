51f59a4414b09d9052acd643f37b4e6b76d657f5
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-22 22:32:27 +0200

    Make CoverPhoto nullable

--- src/Galerie.Core/Entities/Album.cs
+++ src/Galerie.Core/Entities/Album.cs
@@ -7,6 +7,6 @@ public class Album(string title) : EntityBase
 {
     public string Title { get; set; } = Guard.Against.NullOrEmpty(title, nameof(title));
     public string? Description { get; set; }
-    public Photo CoverPhoto { get; set; } = null!;
+    public Photo? CoverPhoto { get; set; }
     public IList<Photo> Photos { get; set; } = new List<Photo>();
 }
