7c55fb0a2526ea2b23eacdaa60893240b31b0c66
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-22 21:54:41 +0200

    Add Photo + Album entities

---
+++ src/Galerie.Core/Entities/Album.cs
@@ -0,0 +1,12 @@
+using Ardalis.GuardClauses;
+using Galerie.Core.Common;
+
+namespace Galerie.Core.Entities;
+
+public class Album(string title) : EntityBase
+{
+    public string Title { get; set; } = Guard.Against.NullOrEmpty(title, nameof(title));
+    public string? Description { get; set; }
+    public Photo CoverPhoto { get; set; } = null!;
+    public IList<Photo> Photos { get; set; } = new List<Photo>();
+}
---
+++ src/Galerie.Core/Entities/Photo.cs
@@ -0,0 +1,14 @@
+﻿using Ardalis.GuardClauses;
+using Galerie.Core.Common;
+
+namespace Galerie.Core.Entities;
+
+public class Photo(string title, string fileName) : EntityBase
+{
+    public string Title { get; set; } = Guard.Against.NullOrEmpty(title, nameof(title));
+    public string? Description { get; set; }
+    public string FileName { get; set; } = Guard.Against.NullOrEmpty(fileName, nameof(fileName));
+
+    public Guid AlbumId { get; set; }
+    public Album Album { get; set; } = null!;
+}
