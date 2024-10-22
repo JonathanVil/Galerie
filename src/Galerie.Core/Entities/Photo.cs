d5cd1efc3c48f59ef5f5aa0edb8e8979c37aa63f
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-22 22:57:59 +0200

    Use URL instead of FileName in Photo

--- src/Galerie.Core/Entities/Photo.cs
+++ src/Galerie.Core/Entities/Photo.cs
@@ -3,11 +3,11 @@ using Galerie.Core.Common;
 
 namespace Galerie.Core.Entities;
 
-public class Photo(string title, string fileName) : EntityBase
+public class Photo(string title, string url) : EntityBase
 {
     public string Title { get; set; } = Guard.Against.NullOrEmpty(title, nameof(title));
     public string? Description { get; set; }
-    public string FileName { get; set; } = Guard.Against.NullOrEmpty(fileName, nameof(fileName));
+    public string Url { get; set; } = Guard.Against.NullOrEmpty(url, nameof(url));
     
     public IList<Album> Albums { get; set; } = new List<Album>();
 }
