eadb196bc5e9f6a071650d66611cf08f4e22fba1
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-22 23:32:59 +0200

    Add Identity support with ApplicationUser entity
    Introduced the ApplicationUser class inheriting IdentityUser to extend identity capabilities. Updated ApplicationDbContext to inherit from IdentityDbContext<ApplicationUser> and included necessary packages in the csproj file for Identity functionality.

--- src/Galerie.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Galerie.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 using System.Reflection;
 using Galerie.Core.Entities;
+using Galerie.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Galerie.Infrastructure.Data;
 
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
     public DbSet<Album> Albums => Set<Album>();
     public DbSet<Photo> Photos => Set<Photo>();
