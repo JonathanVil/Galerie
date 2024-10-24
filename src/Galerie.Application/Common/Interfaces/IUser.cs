4b6593be942018c028c7ab30bea6595729efc4fb
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-24 23:01:00 +0200

    Add identity, event dispatching, and auditing infrastructure
    Introduce identity and authorization services with roles and policies. Implement event dispatching through save changes interceptors. Add auditable entity handling and necessary service registrations in DI setup.

---
+++ src/Galerie.Application/Common/Interfaces/IUser.cs
@@ -0,0 +1,6 @@
+namespace Galerie.Application.Common.Interfaces;
+
+public interface IUser
+{
+    string? Id { get; }
+}
---
+++ src/Galerie.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,56 @@
+using Ardalis.GuardClauses;
+using Galerie.Application.Common.Interfaces;
+using Galerie.Core.Constants;
+using Galerie.Infrastructure.Data;
+using Galerie.Infrastructure.Data.Interceptors;
+using Galerie.Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Galerie.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        Guard.Against.Null(connectionString, message: "Connection string 'DefaultConnection' not found.");
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+        services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+
+            options.UseNpgsql(connectionString);
+        });
+
+        services.AddScoped<ApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+
+        // services.AddScoped<ApplicationDbContextInitialiser>();
+
+        services.AddAuthentication()
+            .AddBearerToken(IdentityConstants.BearerScheme);
+
+        services.AddAuthorizationBuilder();
+
+        services
+            .AddIdentityCore<ApplicationUser>()
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddApiEndpoints();
+
+        services.AddSingleton(TimeProvider.System);
+        services.AddTransient<IIdentityService, IdentityService>();
+
+        services.AddAuthorization(options =>
+            options.AddPolicy(Policies.CanPurge, policy => policy.RequireRole(Roles.Administrator)));
+
+        return services;
+    }
+}
