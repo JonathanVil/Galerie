12ce32ea7698397c00d306e9c28b2b98f2e91758
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-24 22:57:06 +0200

    Add UserId to Photo and Album entities
    This commit introduces a new `UserId` property to both `Photo` and `Album` entities to associate them with the user who created them. The property includes validation to ensure it is not default and is properly initialized.

--- src/Galerie.Core/Entities/Album.cs
+++ src/Galerie.Core/Entities/Album.cs
@@ -2,8 +2,13 @@ using Ardalis.GuardClauses;
 
 namespace Galerie.Core.Entities;
 
-public class Album(string title) : EntityBase
+public class Album(Guid userId, string title) : EntityBase
 {
+    /// <summary>
+    /// The user who owns the album
+    /// </summary>
+    public Guid UserId { get; set; } = Guard.Against.Default(userId, nameof(userId));
+    
     public string Title { get; set; } = Guard.Against.NullOrEmpty(title, nameof(title));
     public string? Description { get; set; }
     public Photo? CoverPhoto { get; set; }
--- src/Galerie.Core/Entities/Photo.cs
+++ src/Galerie.Core/Entities/Photo.cs
@@ -2,8 +2,13 @@
 
 namespace Galerie.Core.Entities;
 
-public class Photo(string title, string url) : EntityBase
+public class Photo(Guid userId, string title, string url) : EntityBase
 {
+    /// <summary>
+    /// The user who uploaded the photo
+    /// </summary>
+    public Guid UserId { get; set; } = Guard.Against.Default(userId, nameof(userId));
+    
     public string Title { get; set; } = Guard.Against.NullOrEmpty(title, nameof(title));
     public string? Description { get; set; }
     public string Url { get; set; } = Guard.Against.NullOrEmpty(url, nameof(url));
