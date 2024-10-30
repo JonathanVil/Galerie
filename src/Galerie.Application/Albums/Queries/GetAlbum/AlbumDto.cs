65098d0fcff3b620ca2486942a038e08d2638f3c
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-31 00:31:40 +0100

    Fix AlbumDto mapper exception

--- src/Galerie.Application/Albums/Queries/GetAlbum/AlbumDto.cs
+++ src/Galerie.Application/Albums/Queries/GetAlbum/AlbumDto.cs
@@ -1,11 +1,17 @@
-
 using Galerie.Core.Entities;
 
 namespace Galerie.Application.Albums.Queries.GetAlbum;
 
-public record AlbumDto(Guid Id, string Title, string? Description, string? CoverUrl, int NumberOfPhotos, DateTime Created, DateTime LastModified)
+public record AlbumDto
 {
     public IReadOnlyCollection<PhotoDto> Photos { get; init; } = Array.Empty<PhotoDto>();
+    public Guid Id { get; init; }
+    public string Title { get; init; } = string.Empty;
+    public string? Description { get; init; }
+    public string? CoverUrl { get; init; }
+    public int NumberOfPhotos { get; init; }
+    public DateTimeOffset Created { get; init; }
+    public DateTimeOffset LastModified { get; init; }
 
     private class Mapping : Profile
     {
@@ -13,7 +19,7 @@ public record AlbumDto(Guid Id, string Title, string? Description, string? Cover
         {
             CreateMap<Album, AlbumDto>()
                 .ForMember(a => a.NumberOfPhotos, opt => opt.MapFrom(s => s.Photos.Count))
-                .ForMember(a => a.CoverUrl, opt => opt.MapFrom((s, _) => s.CoverPhoto?.Url));
+                .ForMember(a => a.CoverUrl, opt => opt.MapFrom(s => s.CoverPhoto != null ? s.CoverPhoto.Url : null));
         }
     }
 }
