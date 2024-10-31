18bd4dca2a501377b5f92a79fdc8774b7a0d29ee
Jonathan Antoine Villeret <jonathan.villeret@gmail.com>
2024-10-31 15:41:42 +0100

    Add Flowbite UI library

--- src/Galerie.Web.Configurator/tailwind.config.js
+++ src/Galerie.Web.Configurator/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{razor,html,cshtml}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./**/*.{razor,html,cshtml}",
+        "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
 }
